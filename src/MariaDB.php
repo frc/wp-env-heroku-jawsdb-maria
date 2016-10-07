84bd1170e4a3ee669b72e633586669e30ad633df
Janne Aalto <janne.aalto@frantic.com>
2016-10-07 18:57:43 +0300

    Initial commit

---
+++ src/MariaDB.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Frc\WP\Env\Heroku\MariaDB;
+
+$env = getenv('JAWSDB_MARIA_URL');
+if ( $env ) {
+    $url = parse_url($env);
+    putenv(sprintf('DB_HOST=%s', $url['host']));
+    putenv(sprintf('DB_PORT=%s', $url['port']));
+    putenv(sprintf('DB_USER=%s', $url['user']));
+    putenv(sprintf('DB_PASSWORD=%s', $url['pass']));
+    putenv(sprintf('DB_NAME=%s', ltrim($url['path'], '/')));
+} else {
+    if (!getenv('DB_HOST')) {
+        putenv('DB_HOST=localhost');
+    }
+    if (!getenv('DB_USER')) {
+        putenv('DB_USER=root');
+    }
+}
