a32b1922437bbf52aa36c589524b249b5bf56915
Hannu Räisänen <hannu.raisanen@frantic.com>
2016-10-19 10:34:53 +0300

    Allow usage of default port without a warning.

--- src/MariaDB.php
+++ src/MariaDB.php
@@ -6,7 +6,9 @@ $env = getenv('JAWSDB_MARIA_URL');
 if ( $env ) {
     $url = parse_url($env);
     putenv(sprintf('DB_HOST=%s', $url['host']));
-    putenv(sprintf('DB_PORT=%s', $url['port']));
+    if ( array_key_exists('port', $url) ) {
+        putenv(sprintf('DB_PORT=%s', $url['port']));
+    }
     putenv(sprintf('DB_USER=%s', $url['user']));
     putenv(sprintf('DB_PASSWORD=%s', $url['pass']));
     putenv(sprintf('DB_NAME=%s', ltrim($url['path'], '/')));
